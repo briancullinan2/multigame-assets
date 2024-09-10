b97a3911c415cd3e7487f44fd1135804dd5416ea
Brian Cullinan <megamindbrian@gmail.com>
2024-09-10 11:35:16 -0700

    "auto-update"

--- maps/compile-maps.sh
+++ maps/compile-maps.sh
@@ -1,5 +1,6 @@
 #bash
 for i in $(find . -iname '*.bsp' -o -iname '*.map'); do
+if [[ ! "$i" =~ concepts/ ]]; then
 if [[ ! "$i" =~ autosave/ ]]; then
 if [[ ! "$i" =~ quarantine/ ]]; then
 
@@ -18,34 +19,38 @@ fi;
 fi;
 
 
+# comment out this line and third fi; to replace existing bsps?
+if [ ! -f "${i%.*}.bsp" ]; then
 if [ -f "${i%.*}.map" ]; then
 if [ $(wc -c <"${i%.*}.map") -le 100000 ]; then
 ~/Quake3e/build/release-darwin-arm/quake3e_q3map2 -meta -keeplights -game quake3 -fs_basepath /Users/briancullinan/planet_quake_data -fs_game quake3-baseq3 "${i%.*}.map";
 ~/Quake3e/build/release-darwin-arm/quake3e_q3map2 -light -fast -game quake3 -fs_basepath /Users/briancullinan/planet_quake_data -fs_game quake3-baseq3 "${i%.*}.bsp";
 fi;
 fi;
+fi;
 
 
-if [ -f "${i%.*}.bsp" ]; then
-if [ $(wc -c <"${i%.*}.bsp") -le 900000 ]; then
-/Applications/Blender.app/Contents/MacOS/Blender --python ../model-convert.py -- "${i%.*}.bsp";
-fi;
-fi;
+# if [ -f "${i%.*}.bsp" ]; then
+# if [ $(wc -c <"${i%.*}.bsp") -le 900000 ]; then
+# /Applications/Blender.app/Contents/MacOS/Blender --python ../model-convert.py -- "${i%.*}.bsp";
+# fi;
+# fi;
 
 # if it still doesn't exist 
 
-if [ ! -f "${i%.*}.md3" ]; then
-if [ ! -f "${i%.*}-half.md3" ]; then
+# if [ ! -f "${i%.*}.md3" ]; then
+# if [ ! -f "${i%.*}-half.md3" ]; then
 
-if [ -f "${i%.*}.map" ]; then
-if [ $(wc -c <"${i%.*}.map") -le 900000 ]; then
-/Applications/Blender.app/Contents/MacOS/Blender --python ../model-convert.py -- "${i%.*}.map";
-fi;
-fi;
+# if [ -f "${i%.*}.map" ]; then
+# if [ $(wc -c <"${i%.*}.map") -le 900000 ]; then
+# /Applications/Blender.app/Contents/MacOS/Blender --python ../model-convert.py -- "${i%.*}.map";
+# fi;
+# fi;
+
+# fi;
+# fi;
 
 fi;
-fi;
-
 fi;
 fi;
 done;
